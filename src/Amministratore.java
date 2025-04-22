a2ecd30be75fd5ea5a5931db24cf53f22fd6b738
vincenzolubrano05 <167540920+vincenzolubrano05@users.noreply.github.com>
2025-04-22 18:57:07 +0200

    Update Amministratore.java
    Modifica

--- src/Amministratore.java
+++ src/Amministratore.java
@@ -1,7 +1,17 @@
-public class Amministratore extends Autenticazione {
-private StatoVolo VoliAmministrati;
-public Amministratore(String nome_utente, String Password) {
-        super(nome_utente,Password);
+public class Amministratore  {
+        private StatoVolo VoliAmministrati;
+        private String nome_amministratore;
+        private String password;
+public Amministratore(StatoVolo VoliAmministrati, String nome_amministratore, String password) {
+        this.VoliAmministrati = VoliAmministrati;
+        this.nome_amministratore = nome_amministratore;
+        this.password = password;
+    }
+        public void accesso(){
+    System.out.println("Accesso effettuato con: "   + nome_utente);
+    }
+public void registrazione(){
+    System.out.println("Registrazione effettuata con: " + nome_utente);
     }
 
 public void InserisciVoli(){
